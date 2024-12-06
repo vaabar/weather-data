acff27ed4e76c61913528419d1df95b184d10c40
vaabar <junaidbarakatarike@gmail.com>
2024-12-06 01:12:45 +0100

    finished the sectiom for search input on javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-function searchButton(event) {}
-let inputElement = document.querySelector("Search-input");
+function searchButton(event) {
+  event.preventDefault();
+  let searchIn = document.querySelector("search-element");
+  let theCityelement = document.querySelector("#the-city");
+  theCityelement.innerHTML = searchIn.value;
+}
+let inputElement = document.querySelector("search-element");
 inputElement.addEventListener("submit", searchButton);
