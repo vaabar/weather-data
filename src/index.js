ad389dc70c00a206d0171837c1bcb5c5f9a291bc
vaabar <junaidbarakatarike@gmail.com>
2024-12-07 23:34:08 +0100

    added thr axious and added my apikeys and api url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "o8c4364da7btfb6f80cb313baf6dceea";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiUrl}&units${metric};
+`}
+
 function searchButton(event) {
   event.preventDefault();
   let searchIn = document.querySelector("search-element");
   let theCityelement = document.querySelector("#the-city");
   theCityelement.innerHTML = searchIn.value;
+  searCity{searchIn.value};
 }
 let inputElement = document.querySelector("search-element");
 inputElement.addEventListener("submit", searchButton);
